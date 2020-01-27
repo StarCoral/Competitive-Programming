abce06ceaf9af7e12df5169091237c3ae2600a2f
StarCoral <qbe841501@gmail.com>
2020-01-28 00:28:09 +0800

    Create b019.cpp
    sort

---
+++ GreenJudge/b019.cpp
@@ -0,0 +1,23 @@
+#include<algorithm>
+#include<iostream>
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    for(int i = 0 ; i < n ; i++){
+        cin>>a[i];
+    }
+    sort(a.rbegin(),a.rend());
+    for(int i = 0 ; i < n ; i++){
+        cout<<a[i]<<' ';
+    }
+
+}
+
+ 
+
